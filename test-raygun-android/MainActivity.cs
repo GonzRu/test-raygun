0cc6cf837353ebede0c9531c9645acd50af9e8a3
Smirnov Denis <gonzru@gmail.com>
2013-11-12 12:35:42 +0400

    В android-проект добавлены 4 кнопки, при нажатии на которые выбрасываются и обрабатываются исключения

--- test-raygun-android/MainActivity.cs
+++ test-raygun-android/MainActivity.cs
@@ -5,29 +5,99 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading.Tasks;
 
 namespace testraygunandroid
 {
 	[Activity (Label = "test-raygun-android", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		private Button _btn1;
+		private Button _btn2;
+		private Button _btn3;
+		private Button _btn4;
 
 		protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			var layot = new LinearLayout(this);
+			layot.Orientation = Orientation.Vertical;
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			_btn1 = new Button(this);
+			_btn1.SetText(Resource.String.Button1Text);
+			_btn1.Click += OnClickButton1;
+
+			_btn2 = new Button(this);
+			_btn2.SetText(Resource.String.Button2Text);
+			_btn2.Click += OnClickButton2;
+
+			_btn3 = new Button(this);
+			_btn3.SetText(Resource.String.Button3Text);
+			_btn3.Click += OnClickButton3;
+
+			_btn4 = new Button(this);
+			_btn4.SetText(Resource.String.Button4Text);
+			_btn4.Click += OnClickButton4;
+
+			layot.AddView(_btn1);
+			layot.AddView(_btn2);
+			layot.AddView(_btn3);
+			layot.AddView(_btn4);
+			SetContentView(layot);
 		}
+
+		#region Button Handlers
+		private void OnClickButton1(object sender, EventArgs e)
+		{
+			try
+			{
+				throw new Exception("Exception from Button1");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Catch Exception:" + ex.Message);
+			}
+		}
+
+		private void OnClickButton2(object sender, EventArgs e)
+		{
+			try
+			{
+				throw new ArgumentNullException("ArgumentNullException from Button2");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Catch Exception:" + ex.Message);
+			}
+		}
+
+		private void OnClickButton3(object sender, EventArgs e)
+		{
+			try
+			{
+				throw new OutOfMemoryException("OutOfMemoryException from Button3");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Catch Exception:" + ex.Message);
+			}
+		}
+
+		private void OnClickButton4(object sender, EventArgs e)
+		{
+			Task.Run( () => {
+				try
+				{
+					throw new Exception("Exception from Button4 from new thread");
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Catch Exception:" + ex.Message);
+				}
+			});
+		}
+		#endregion
 	}
 }
 
